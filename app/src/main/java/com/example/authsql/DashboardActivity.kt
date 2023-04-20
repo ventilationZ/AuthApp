4875b080dd6b777c0b2394d08cf86ecbf44ab029
eMobilis <elikibetael4@gmail.com>
2023-04-20 10:47:20 +0300

    fihgfhewsfd

--- app/src/main/java/com/example/authsql/DashboardActivity.kt
+++ app/src/main/java/com/example/authsql/DashboardActivity.kt
@@ -1,11 +1,20 @@
 package com.example.authsql
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var BtnLogout:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        BtnLogout=findViewById(R.id.btnlogout)
+
+        BtnLogout.setOnClickListener {
+            var gotologout = Intent(this,MainActivity::class.java)
+            startActivity(gotologout)
+        }
     }
 }
